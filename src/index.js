84d4528a47802da399308fde16c9823a923f80d5
raikaat <raikaat@gmail.com>
2017-09-25 23:26:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   var arr = str.split("");
+    var count = 0;
+    var count2 = 0;
+    var count3 = 0
+    
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === "(")  {count++};
+        if (arr[i] === "[") {count2++};
+        if (arr[i] === "{") {count3++};
+        if (arr[i] === ")") {count--};
+        if (arr[i] === "]") {count2--};
+        if (arr[i] === "}") {count3--}
+        if ((count < 0) || (count2 < 0) ||(count3 < 0)) {return false};
+        
+    }
+    
+    return ((count === 0) && (count2 === 0) & (count3 === 0))  ? true : false;
 }
